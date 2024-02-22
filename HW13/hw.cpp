567d5afd31cc2302c7b123c52a146c3d99392fe0
bilotti26 <m260516@usna.edu>
2024-02-22 13:25:45 -0500

    msg

---
+++ HW13/hw.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    while( cin )
+    {
+      int a = 100;
+      cin >> a;
+      cout << a << endl;
+    
+      int b = 200;
+      cin >> b;
+      cout << b << endl;
+    }
+}
